4e3925a0900b5f5d077ed951e009b9ab3b5ebe3a
RonanKer29 <kervella.ronan@hotmail.com>
2024-05-09 15:06:49 +0200

    dynamic content for tabs

--- code/03 React Essentials/01-starting-project/src/App.jsx
+++ code/03 React Essentials/01-starting-project/src/App.jsx
@@ -4,6 +4,13 @@ import CoreConcepts from "./components/CoreConcept.jsx";
 import TabButton from "./components/TabButton.jsx";
 
 function App() {
+  let tabContent = "pls click a btn";
+
+  function handleSelect(selectedButton) {
+    tabContent = selectedButton;
+    console.log(selectedButton);
+  }
+
   return (
     <div>
       <Header />
@@ -24,11 +31,14 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton>Components</TabButton>
-            <TabButton>JSX</TabButton>
-            <TabButton>Props</TabButton>
-            <TabButton>State</TabButton>
+            <TabButton onSelect={() => handleSelect("components")}>
+              Components
+            </TabButton>
+            <TabButton onSelect={() => handleSelect("jsx")}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect("props")}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect("state")}>State</TabButton>
           </menu>
+          {tabContent}
         </section>
       </main>
     </div>
