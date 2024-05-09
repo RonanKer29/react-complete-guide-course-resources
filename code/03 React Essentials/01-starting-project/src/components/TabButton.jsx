95191061515bf204b109397ff8b617ca29dabe8f
RonanKer29 <kervella.ronan@hotmail.com>
2024-05-09 14:22:07 +0200

    multiple files per components

---
+++ code/03 React Essentials/01-starting-project/src/components/TabButton.jsx
@@ -0,0 +1,11 @@
+export default function TabButton({ children }) {
+  function handleClick() {
+    console.log("Button clicked");
+  }
+
+  return (
+    <li>
+      <button onClick={handleClick}>{children}</button>
+    </li>
+  );
+}
